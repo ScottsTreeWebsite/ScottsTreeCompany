7935ee038dbc88a77088c6a811b2241a7724a3b2
ScottsTreeWebsite <149089710+ScottsTreeWebsite@users.noreply.github.com>
2023-10-26 12:14:33 -0400

    Update to code to work with website url

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function TravelToPage(Page){
     
     if(Page == 'About Us'){
-        window.location="file:///C:/Users/Damion/Documents/.NetProjects/ScottsTreeCompanySite/pages/About%20Us/AboutIndex.html"
+        window.location="https://scottstreewebsite.github.io/ScottsTreeCompany//pages/About%20Us/AboutIndex.html"
         console.log("About Us Page");
     }
     else if(Page == "Home"){
-        window.location="file:///C:/Users/Damion/Documents/.NetProjects/ScottsTreeCompanySite/index.html"
+        window.location="https://scottstreewebsite.github.io/ScottsTreeCompany/"
         console.log("Home Page");
     }
     else if(Page == "Facebook"){
-        window.location.href='www.facebook.com/profile.php?id=100094753545629';
+        window.location.href='https://www.facebook.com/profile.php?id=100094753545629';
         console.log("Facebook Page");
     }
 }
